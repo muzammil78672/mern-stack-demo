4d55a0559adb301c8946f7a969c66c12a3b02183
muzammil hussain chandorewala <muzammil.chandorewala@northout.com>
2020-06-12 12:22:58 +0530

    update url

--- src/frontEnd/config/request.js
+++ src/frontEnd/config/request.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3005/api",
+  // baseURL: "http://localhost:3005/api",
+  baseURL: "https://quiet-crag-62652.herokuapp.com/api",
 });
 
 axios.interceptors.response.use(
